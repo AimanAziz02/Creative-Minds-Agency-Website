4c0d957f22c9e3e3b72f425442b046acc30ae5ab
Batool-Zahra <batoolzahrar@gmail.com>
2024-11-06 00:40:23 +0500

    Add JavaScript file for Creative Minds Agency Website

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    const introElement = document.getElementById("agency-intro");
+    if (introElement) {
+        introElement.innerText = `
+            Welcome to Creative Minds Agency! We are a full-service creative agency
+            specializing in branding, digital marketing, and innovative design solutions.
+            Our team of passionate professionals brings ideas to life, crafting unique
+            and memorable experiences that captivate audiences and drive results.
+            Let’s create something amazing together!
+        `;
+    }
+};
+
